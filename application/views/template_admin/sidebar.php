0322b1a07c98782c62642e32d3d7db45625e18f4
Salsabila Marhan <sabilamarhan11@gmail.com>
2022-08-04 01:52:23 +0700

    Merubah nama Perusahaan

--- application/views/template_admin/sidebar.php
+++ application/views/template_admin/sidebar.php
@@ -101,7 +101,7 @@
             <i class="fa fa-bars"></i>
           </button>
 
-          <h4 class="font-weight-bold">PT. Kelompok SABIL</h4>
+          <h4 class="font-weight-bold">PT. Unikom Kelompok Full Cewek</h4>
 
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
--- application/views/template_pegawai/sidebar.php
+++ application/views/template_pegawai/sidebar.php
@@ -64,7 +64,7 @@
             <i class="fa fa-bars"></i>
           </button>
 
-          <h4 class="font-weight-bold">PT. Kelompok SABIL</h4>
+          <h4 class="font-weight-bold">PT. Unikom Kelompok Full Cewek</h4>
 
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
